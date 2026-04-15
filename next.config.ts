fd08d64627bfb52b752c8f2e534aa2514b7ce280
onepeso <giovaniportal@gmail.com>
2026-04-14 22:11:16 -0700

    Fixed some errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  // 1. Enable Static Export
+  output: "export",
+
+  // 2. Mandatory for static exports in Next.js
+  images: {
+    unoptimized: true,
+  },
+
   turbopack: {
     root: path.resolve(__dirname),
   },
